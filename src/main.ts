c554395cc9d47284dfc899184478125498cee9da
Rastislav <rastislav@Rastislavs-MBP.home>
2024-09-06 01:17:31 +0200

    add swagger config

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -11,6 +12,15 @@ async function bootstrap() {
         methods: ['GET', 'POST'],
         credentials: true
     });
+
+    const config = new DocumentBuilder()
+        .setTitle('Souls BE API Documentation')
+        .setDescription('All Souls BE API endpoints documented below')
+        .setVersion('1.0')
+        .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('swagger', app, document);
 
     const configService = app.get(ConfigService);
     const port = configService.get('APP_PORT');
